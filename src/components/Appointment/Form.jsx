7a6413eb1945b6bad809077a1dcde068bee439b5
vierge <marancholia@gmail.com>
2020-05-13 15:49:45 -0400

    added default values to state hooks

--- src/components/Appointment/Form.jsx
+++ src/components/Appointment/Form.jsx
@@ -10,8 +10,8 @@ export default function Form(props) {
   const { interviewers } = props; // value properties
   const { onSave, onCancel } = props; // action properties
 
-  const [name, setName] = useState("")
-  const [interviewer, setInterviewer] = useState(0)
+  const [name, setName] = useState(props.name || "")
+  const [interviewer, setInterviewer] = useState(props.interviewer || 0)
 
   return (
     <main className="appointment__card appointment__card--create">
