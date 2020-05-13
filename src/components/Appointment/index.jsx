8dc589f7719d7c6edd57b47b4beabf97c7a20316
vierge <marancholia@gmail.com>
2020-05-13 15:24:45 -0400

    Moved and fixed appointment components

---
+++ src/components/Appointment/index.jsx
@@ -0,0 +1,25 @@
+// react imports
+import React, { useState } from 'react'
+
+// styles imports
+import "./styles.scss"
+
+// component import
+import Confirm from "./Confirm"
+import Empty from "./Empty"
+import Error from "./Error"
+import Header from "./Header"
+import Show from "./Show"
+import Status from "./Status"
+import Button from "../Button"
+
+
+export default function Appointment(props) {
+
+
+  return (
+    <article className="appointment">
+
+    </article>
+  )
+}
