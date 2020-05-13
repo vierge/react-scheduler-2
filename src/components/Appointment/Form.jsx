377a402e9778f5f94436bc3f78653f14b49481de
vierge <marancholia@gmail.com>
2020-05-13 16:10:29 -0400

    fixed Form.jsx submission bugs

--- src/components/Appointment/Form.jsx
+++ src/components/Appointment/Form.jsx
@@ -11,18 +11,28 @@ export default function Form(props) {
   const { onSave, onCancel } = props; // action properties
 
   const [name, setName] = useState(props.name || "")
-  const [interviewer, setInterviewer] = useState(props.interviewer || 0)
+  const [interviewer, setInterviewer] = useState(props.interviewer || null)
+
+
+  function cancel() {
+    function reset() {
+      setInterviewer(null);
+      setName("");
+    }
+    reset()
+    onCancel();
+  }
 
   return (
     <main className="appointment__card appointment__card--create">
       <section className="appointment__card-left">
-        <form autoComplete="off" onSubmit={setName}>
+        <form autoComplete="off" onSubmit={event => event.preventDefault()}>
           <input
             className="appointment__create-input text--semi-bold"
-            name={name}
+            value={name}
             type="text"
             placeholder="Enter Student Name"
-            onChange={event => setName(event)}
+            onChange={event => setName(event.target.value)}
           /*
             This must be a controlled component
           */
@@ -32,7 +42,7 @@ export default function Form(props) {
       </section>
       <section className="appointment__card-right">
         <section className="appointment__actions">
-          <Button danger onClick={onCancel}>Cancel</Button>
+          <Button danger onClick={(event) => cancel()}>Cancel</Button>
           <Button confirm onClick={onSave}>Save</Button>
         </section>
       </section>
