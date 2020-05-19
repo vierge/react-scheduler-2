28f345d456f07166edbe0095b916d4c32706db7d
vierge <marancholia@gmail.com>
2020-05-19 19:42:28 -0400

    a reset undid a bugfix. fixed it

--- src/components/Appointment/Show.jsx
+++ src/components/Appointment/Show.jsx
@@ -4,6 +4,8 @@ export default function Show(props) {
 
   const { student, interviewer, onEdit, onDelete } = props;
 
+  console.log(interviewer);
+
   return (
     <main className="appointment__card appointment__card--show">
       <section className="appointment__card-left">
--- src/components/Appointment/index.jsx
+++ src/components/Appointment/index.jsx
@@ -9,18 +9,29 @@ import Header from "./Header"
 import Show from "./Show"
 // import Form from "./Form"
 import Empty from "./Empty"
-
+import useVisualMode from "hooks/useVisualMode"
 
 export default function Appointment(props) {
 
-  const { interview } = props;
+
+
+  const EMPTY = "EMPTY";
+  const SHOW = "SHOW";
+
+
+  const { interview, onAdd } = props;
+
+  console.log(interview);
+  const { mode, transition, back } = useVisualMode(
+    interview ? SHOW : EMPTY
+  )
 
   return (
     <article className="appointment" key={interview}>
       <header><Header /></header>
-      {interview ?
-        <Show student={interview.student} interviewer={interview.interviewer.name} /> :
-        <Empty />}
+      {mode === EMPTY && <Empty onAdd={onAdd} />}
+      {mode === SHOW && <Show student={interview.student} interviewer={interview.interviewer.name} />}
+
     </article>
   )
 }
