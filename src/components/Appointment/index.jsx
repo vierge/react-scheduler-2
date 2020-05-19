907d5f2d3e8f4f7e07fc801026db9c357ec1c9ea
vierge <marancholia@gmail.com>
2020-05-19 19:53:22 -0400

    began implementing useVisualMode in the Appointment component

--- src/components/Appointment/index.jsx
+++ src/components/Appointment/index.jsx
@@ -10,6 +10,7 @@ import Show from "./Show"
 // import Form from "./Form"
 import Empty from "./Empty"
 import useVisualMode from "hooks/useVisualMode"
+import Form from './Form'
 
 export default function Appointment(props) {
 
@@ -17,20 +18,25 @@ export default function Appointment(props) {
 
   const EMPTY = "EMPTY";
   const SHOW = "SHOW";
+  const CREATE = "CREATE";
 
 
-  const { interview, onAdd } = props;
+  const { interview, interviewers } = props;
+
+
 
   console.log(interview);
   const { mode, transition, back } = useVisualMode(
     interview ? SHOW : EMPTY
   )
 
+
   return (
     <article className="appointment" key={interview}>
       <header><Header /></header>
-      {mode === EMPTY && <Empty onAdd={onAdd} />}
+      {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
       {mode === SHOW && <Show student={interview.student} interviewer={interview.interviewer.name} />}
+      {mode === CREATE && <Form interviewers={[]} onCancel={() => back()} />}
 
     </article>
   )
